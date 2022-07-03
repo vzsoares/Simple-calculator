6ba2d2e3dbd2e70ec7cae834ea7100b7054b7c74
vzsoares <jogolots2@gmail.com>
2022-07-03 18:46:30 -0300

    small improvements

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-// calculadora
-
 const button = document.getElementById("submit");
 
 button.addEventListener("click", () => {
   const operation = document.getElementById("operation").value;
-  const number = document.getElementById("numero").value;
-  const number2 = document.getElementById("numero2").value;
-  const resultadoDiv = document.getElementById("resultados");
+  const number = document.getElementById("number").value;
+  const number2 = document.getElementById("number2").value;
+  const resultadoDiv = document.getElementById("result");
 
-  resultadoDiv.innerHTML = `<h2>Resultado: ${eval(
+  resultadoDiv.innerHTML = `<h2>Result: ${eval(
     number + operation + number2
   )}</h2>`;
 });
